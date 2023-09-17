f1c273e226ab581760a51277642730d996a894bc
kbirson <kbirson@gmail.com>
2023-09-17 15:49:30 -0400

    deleting notes file. this info is in the slides

--- devtools_setup_notes.R
+++
@@ -1,8 +0,0 @@
-library(devtools)
-#create_package("~/libminer")
-use_git_config(user.name="kbirson",user.email="kbirson@gmail.com")
-git_sitrep()
-usethis::use_devtools()
-
-
-usethis::use_r("lib-summary") #creating a function to summarize library contents

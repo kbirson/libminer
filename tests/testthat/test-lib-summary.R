86a9c56d4e6de16cd5ececa4e9b17bc0abf22b27
kbirson <kbirson@gmail.com>
2023-09-17 15:45:56 -0400

    fixed case for "library"

--- tests/testthat/test-lib-summary.R
+++ tests/testthat/test-lib-summary.R
@@ -2,7 +2,7 @@ test_that("lib_summary returns expected result", {
   res <- libsummary() #call function
   expect_s3_class(res, "data.frame") #should return a df
   expect_equal(ncol(res),2) #should have 2 cols
-  expect_equal(names(res),c("Library", "n_packages"))
+  expect_equal(names(res),c("library", "n_packages"))
   expect_type(res$Library,"character")
   expect_type(res$n_packages, "integer")
 })
